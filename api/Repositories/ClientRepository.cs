f0f530c1457785d3399d93726a7cb6c14e9257e3
rylanarguelles <rylan@prosperna.com>
2022-09-30 22:49:07 +0800

    initial commit
    Link to original repository: https://github.com/Carepatron/Carepatron-Test-Full

---
+++ api/Repositories/ClientRepository.cs
@@ -0,0 +1,50 @@
+﻿using api.Data;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repositories
+{
+    public interface IClientRepository
+    {
+        Task<Client[]> Get();
+        Task Create(Client client);
+        Task Update(Client client);
+    }
+
+    public class ClientRepository : IClientRepository
+    {
+        private readonly DataContext dataContext;
+
+        public ClientRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        public async Task Create(Client client)
+        {
+            await dataContext.AddAsync(client);
+            await dataContext.SaveChangesAsync();
+        }
+
+        public Task<Client[]> Get()
+        {
+            return dataContext.Clients.ToArrayAsync();
+        }
+
+        public async Task Update(Client client)
+        {
+            var existingClient = await dataContext.Clients.FirstOrDefaultAsync(x => x.Id == client.Id);
+
+            if (existingClient == null)
+                return;
+
+            existingClient.FirstName = client.FirstName;
+            existingClient.LastName = client.LastName;
+            existingClient.Email = client.Email;
+            existingClient.PhoneNumber = client.PhoneNumber;
+
+            await dataContext.SaveChangesAsync();
+        }
+    }
+}
+
